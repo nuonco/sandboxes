d4e0bb5e6d19f43fc8bf4b7490ad97911885f368
Jon Morehouse <morehousej09@gmail.com>
2024-02-16 09:22:20 -0600

    update

--- aws-ecs/vpc.tf
+++ aws-ecs/vpc.tf
@@ -11,7 +11,7 @@ locals {
       # you can see the math for this /16 here https://www.davidc.net/sites/default/subnets/subnets.html?network=10.128.0.0&mask=16&division=23.ff3100
       public_subnets  = ["10.128.0.0/26", "10.128.0.64/26", "10.128.0.128/26"]
       private_subnets = ["10.128.128.0/24", "10.128.129.0/24", "10.128.130.0/24"]
-      database_subnets  = ["10.129.0.0/26", "10.129.0.64/26", "10.129.0.128/26"]
+      database_subnets = ["10.128.131.0/24"]
     }
   }
 }
