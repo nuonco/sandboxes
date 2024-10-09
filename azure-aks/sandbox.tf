0b7f39e1ed881807e9513f4682dd0199fbf6e2f1
Jordan Acosta <jordanjacosta@gmail.com>
2024-10-09 15:38:03 -0700

    chore(azure-aks): test sandbox ref

--- azure-aks/sandbox.tf
+++ azure-aks/sandbox.tf
@@ -1,7 +1,7 @@
 module "sandbox" {
-  # source = "nuonco/aks-sandbox/azure"
-  # version = "1.3.12"
-  source = "github.com/nuonco/terraform-azure-aks-sandbox?ref=ja%2F6033-add-tag-policy"
+  source  = "nuonco/aks-sandbox/azure"
+  version = "1.3.12"
+  # source = "github.com/nuonco/terraform-azure-aks-sandbox?ref=ja%2F6033-add-tag-policy"
 
   location             = var.location
   nuon_id              = var.nuon_id
