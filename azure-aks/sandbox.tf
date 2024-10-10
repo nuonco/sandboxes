cd4cce6711ca33e2ffb35414e5deb35c18542b64
Jordan Acosta <jordanjacosta@gmail.com>
2024-10-10 10:07:41 -0700

    chore(azure-aks): update sandbox version

--- azure-aks/sandbox.tf
+++ azure-aks/sandbox.tf
@@ -1,7 +1,7 @@
 module "sandbox" {
-  source  = "nuonco/aks-sandbox/azure"
-  version = "1.3.12"
-  # source = "github.com/nuonco/terraform-azure-aks-sandbox?ref=ja%2F6033-add-tag-policy"
+  #source  = "nuonco/aks-sandbox/azure"
+  #version = "1.3.12"
+  source = "github.com/nuonco/terraform-azure-aks-sandbox?ref=ja%2F6033-add-tag-policy"
 
   location             = var.location
   nuon_id              = var.nuon_id
@@ -10,4 +10,5 @@ module "sandbox" {
   cluster_version      = var.cluster_version
   vm_size              = var.vm_size
   node_count           = var.node_count
+  tags                 = var.tags
 }
