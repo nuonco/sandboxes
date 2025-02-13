218249d0e9198511411c6d8a3749c99181883890
fidiego <sayheytofred@gmail.com>
2025-02-13 09:29:28 -0600

    feat: exp: use v of module w/ inferentia ng

--- aws-eks/sandbox.tf
+++ aws-eks/sandbox.tf
@@ -1,8 +1,10 @@
 module "sandbox" {
   # NOTE(fd): example format for testing branches
   # source = "github.com/nuonco/terraform-aws-eks-sandbox?ref=e4c8e0feda3b80c84bb32a170f8969194160b621"
-  source  = "nuonco/eks-sandbox/aws"
-  version = "1.4.4"
+  # source  = "nuonco/eks-sandbox/aws"
+  # version = "1.4.4"
+  #
+  source = "github.com/nuonco/terraform-aws-eks-sandbox?ref=394364048b9159a1a23c5e6cc38d0cba82642899"
 
   install_name          = var.install_name
   cluster_name          = var.cluster_name
