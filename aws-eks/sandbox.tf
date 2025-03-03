3a30fe403f4b93a6a1304e926d491863722a269b
fidiego <sayheytofred@gmail.com>
2025-03-02 20:33:40 -0600

    fix: wip - point to branch with odr iam role fix

--- aws-eks/sandbox.tf
+++ aws-eks/sandbox.tf
@@ -1,8 +1,9 @@
 module "sandbox" {
   # NOTE(fd): example format for testing branches
-  # source = "github.com/nuonco/terraform-aws-eks-sandbox?ref=e4c8e0feda3b80c84bb32a170f8969194160b621"
-  source  = "nuonco/eks-sandbox/aws"
-  version = "1.4.7"
+  # source  = "nuonco/eks-sandbox/aws"
+  # version = "1.4.7"
+  #
+  source = "github.com/nuonco/terraform-aws-eks-sandbox?ref=721f84f304038c02e28a89ed2afbea53ac7f7f02"
 
   install_name          = var.install_name
   cluster_name          = var.cluster_name
