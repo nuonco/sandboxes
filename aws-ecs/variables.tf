45f479eef353f68c36b6ff22fb8e92c18b3699b6
Tim Wong <tim@nuon.co>
2025-05-19 13:46:33 -0700

    removed install-role

--- aws-ecs/variables.tf
+++ aws-ecs/variables.tf
@@ -24,30 +24,3 @@ variable "tags" {
   type        = map(any)
   description = "List of custom tags to add to the install resources. Used for taxonomic purposes."
 }
-
-variable "region" {
-  type        = string
-  description = "The region to launch the cluster in"
-}
-
-// NOTE: if you would like to create an internal load balancer, with TLS, you will have to use the public domain.
-variable "internal_root_domain" {
-  type        = string
-  description = "internal root domain."
-}
-
-variable "public_root_domain" {
-  type        = string
-  description = "public root domain."
-}
-
-variable "runner_install_role" {
-  type        = string
-  description = "The role that is used to install the runner, and should be granted access."
-}
-
-variable "enable_public_route53_zone" {
-  type        = string
-  default     = "true"
-  description = "Provision a public Route53 zone."
-}
