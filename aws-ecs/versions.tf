e5404b0cd841dde4ac10fb361d01c4a3331454fc
Tim Wong <tim@nuon.co>
2025-05-19 14:28:19 -0700

    removed the other providers

--- aws-ecs/versions.tf
+++ aws-ecs/versions.tf
@@ -6,17 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.86.1"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.4"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.13.1"
-    }
   }
 }
